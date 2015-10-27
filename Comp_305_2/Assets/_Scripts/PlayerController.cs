2651b6a41cdd365b1845c229741706e30227867b
nem2015 <nem.cencol@gmail.com>
2015-10-26 23:32:32 -0400

    Added coin Controller + Player Controller

---
+++ Comp_305_2/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    //Private Instance Variables
+    private AudioSource[] _audioSources;
+    private AudioSource _coinSound;
+	// Use this for initialization
+	void Start () {
+        this._audioSources = gameObject.GetComponents<AudioSource>();
+        this._coinSound = this._audioSources[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter2D(Collision2D otherCollider)
+    {
+        if (otherCollider.gameObject.CompareTag("coin"))
+        {
+            this._coinSound.Play();
+        }
+    }
+}
